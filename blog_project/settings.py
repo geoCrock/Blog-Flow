491306b5624d49273412e6218ea3fa58128aec11
geoCrock <georgiy.pypok1337@gmail.com>
2023-12-29 18:28:14 +0300

    removed config

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -11,7 +11,18 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from .config import *
+import os
+from dotenv import load_dotenv
+
+# Import environment
+load_dotenv()
+
+POSTGRES_NAME = os.environ.get('POSTGRES_NAME')
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
